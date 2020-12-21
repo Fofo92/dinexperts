afd93b19e10b70d6c6639daad98976cf67768b50
Pascal Fodiman <pascal@fodiman.fr>
2020-12-21 22:14:09 +0100

    Seed experts data

--- app/models/expert.rb
+++ app/models/expert.rb
@@ -2,6 +2,7 @@ class Expert < ApplicationRecord
   belongs_to :title
   validates :last_name, presence: true
   validates :first_name, presence: true
+  validates :last_name, uniqueness: { scope: [:first_name, :title_id] }
 
   def full_name
     "#{self.first_name} #{self.last_name}"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
 
 puts 'Start seeding titles'
 Title.create(short_title: 'M.', long_title: 'Monsieur')
@@ -12,3 +13,19 @@ Title.create(short_title: 'Mme', long_title: 'Madame')
 Title.create(short_title: 'Dr.', long_title: 'Docteur')
 Title.create(short_title: 'Pr.', long_title: 'Professeur')
 puts 'Seeding titles ended'
+
+
+# INSERT INTO experts (title_id, last_name, first_name, comment, created_at, updated_at) VALUES
+filepath = 'db/seed_data/experts.csv'
+
+puts ''
+puts 'Start seeding experts'
+CSV.foreach(filepath) do |row|
+  if Expert.create(title_id: row[0], last_name: row[1], first_name: row[2])
+    puts "Expert: #{row[0]}, #{row[1]} -> OK "
+  else
+    puts "Expert: #{row[0]}, #{row[1]} -> NOK "
+  end
+puts 'End seeding experts'
+end
+
