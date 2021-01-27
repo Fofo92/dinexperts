f4449e5247e0329896b8463d7d4bc03a173ac3cd
Pascal Fodiman <pascal@fodiman.fr>
2021-01-27 17:59:06 +0100

    add validation of dates in Body and Membership

--- app/models/body.rb
+++ app/models/body.rb
@@ -3,6 +3,7 @@ class Body < ApplicationRecord
   has_many :experts, through: :memberships
   validates :long_name, presence: true, uniqueness: true
   validates :short_name, presence: true
+  validate :ending_date_after_starting_date
   has_ancestry
 
   def full_short_name
@@ -23,5 +24,15 @@ class Body < ApplicationRecord
 
   def full_name
     "#{self.full_short_name}: #{self.long_name}"
+  end
+
+  private
+
+  def ending_date_after_starting_date
+    return if ending_date.blank? || starting_date.blank?
+
+    if :ending_date < :starting_date
+      errors.add :ending_date, "Doit être postérieure à la date de début de période"
+    end
   end
 end
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -5,4 +5,15 @@ class Membership < ApplicationRecord
   validates :membership_type, presence: true
   validates :body_id, presence: true
   validates :expert_id, presence: true
+  validate :ending_at_after_starting_at
+
+  private
+
+  def ending_at_after_starting_at
+    return if ending_at.blank? || starting_at.blank?
+
+    if :ending_at < :starting_at
+      errors.add :ending_at, "Doit être postérieure à la date de début de période"
+    end
+  end
 end
